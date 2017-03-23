45e11288ba24aab678451b230a5ef2a314a6fe10
Dave E Smith <desmith@gmail.com>
2017-03-23 11:10:44 -0400

    added SECRET_KEY generator

---
+++ scripts/gen_secret_key.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import random
+
+
+SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+#secret = file(SECRET_FILE, 'w')
+#secret.write(SECRET_KEY)
+#secret.close()
+
+print "SECRET_KEY = '%s" % SECRET_KEY
