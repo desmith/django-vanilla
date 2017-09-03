36b8e7eeff5ce46f8f9c0e7654eef1aac8ceb6e5
Dave E Smith <desmith@gmail.com>
2017-09-02 20:08:24 -0400

    added flow

--- react/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from 'App';
-
-let rootNode = document.getElementById('app_node');
-ReactDOM.render(
-  <App />,
-  rootnode
-);
--- react/javascripts/example.js
+++ react/javascripts/example.js
@@ -1,2 +1,11 @@
+import ReactDOM from 'react-dom';
+import App from 'App';
+
+let rootNode = document.getElementById('app_node');
+ReactDOM.render(
+  <App />,
+  rootnode
+);
+
 
 console.log("{{project_name}}.js loaded");
