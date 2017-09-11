5756ab97bf15704517f4e7556a0542bdd04b7980
Dave E Smith <desmith@gmail.com>
2017-09-11 17:38:45 -0400

    reorg

---
+++ javascript/example.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import App from 'App';
+
+let rootNode = document.getElementById('app_node');
+ReactDOM.render(
+  <App />,
+  rootNode
+);
+
+
+console.log("{{project_name}}.js loaded");
